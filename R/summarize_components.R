64adbd6a8f9ec1781e3bf1e586b2a2366005e6b2
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-02-18 19:56:27 +0100

    added rmarkdown skeleton and basis for functions

---
+++ R/summarize_components.R
@@ -0,0 +1,16 @@
+# summarize_components ####
+#
+#
+#' give_general_summary Describes size, rows, columns, groupings, rownames, variablenames
+#'
+#' Collects information about the data.frame and stores them.
+#'
+#' @param dat Name of data.frame
+#' @export
+# give_general_summary(dat= dat) {
+#         size<-object.size(dat)
+#         rownames<-row.names(dat)
+#         varnames<-names(dat)
+#         if(is.grouped_df(dat)==TRUE){grouplabels<-attr(dat, "labels")}
+#
+# }  #seems easier to just put in rmarkdownfile
---
+++ R/summarize_dat.R
@@ -0,0 +1,9 @@
+#' Summarizedat Will Summarize Your Datafile In a Nice and Clean Way
+#'
+#' Once you cleaned and tested your tidy dataframe, you should create a summary of the
+#' file. This package will act as a nicely formated str() and summary()
+#' but will also perform as a codebook.
+#'
+#'  @docType package
+#'  @name summarizedat
+NULL
